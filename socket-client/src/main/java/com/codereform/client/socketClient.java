fbe33301085a1b000ec585c1d8126a580aa29a35
gdyrrahitis <giorgos.dyrrahitis@gmail.com>
2021-02-24 18:42:19 +0000

    feat: added GUI for socket client

--- socket-client/src/main/java/com/codereform/client/socketClient.java
+++ socket-client/src/main/java/com/codereform/client/socketClient.java
@@ -3,11 +3,23 @@ package com.codereform.client;
 import com.codereform.custom.socket.ResponseContext;
 import com.codereform.custom.socket.ShutdownCommandHandler;
 
+import javax.swing.*;
+
 public class socketClient {
     public static void main(String[] args) {
-        var handler = new ShutdownCommandHandler();
-        var response = handler.Handle(null);
-        var context = new ResponseContext(response);
-        context.executeResponse();
+        var frame = new JFrame();
+        var button = new JButton("Click");
+        button.setBounds(130, 100, 100, 40);
+        button.addActionListener(e -> {
+            frame.invalidate();
+        });
+        frame.add(button);
+        frame.setSize(400, 500);
+        frame.setLayout(null);
+        frame.setVisible(true);
+//        var handler = new ShutdownCommandHandler();
+//        var response = handler.Handle(null);
+//        var context = new ResponseContext(response);
+//        context.executeResponse();
     }
 }
